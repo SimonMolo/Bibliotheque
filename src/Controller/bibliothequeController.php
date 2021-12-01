6468b6f94b463513e860e3f638dc2a2b8bb0aa52
SimonMolo <lemartelotsimon@gmail.com>
2021-12-01 16:45:27 +0100

    pages accueil livres et livre presque sans css

--- src/Controller/bibliothequeController.php
+++ src/Controller/bibliothequeController.php
@@ -2,7 +2,103 @@
 
 namespace App\Controller;
 
-class bibliothequeController
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class bibliothequeController extends abstractcontroller
 {
+    /**
+     * @Route("/", name="home")
+     */
+    public function accueil()
+    {
+        return $this->render('home.html.twig');
+    }
+
+    /**
+     * @Route("/livres", name="livres")
+     */
+    public function livres()
+    {
+        $livres = [
+            1 => [
+                "title" => "Dune",
+                "author" => "Franck Herbert",
+                "publishedAt" => new \DateTime('NOW'),
+                "id" => 1
+            ],
+            2 => [
+                "title" => "Silo",
+                "author" => "Tery Hayes",
+                "publishedAt" => new \DateTime('NOW'),
+                "id" => 2
+            ],
+            3 => [
+                "title" => "Win",
+                "author" => "Harlan Coben",
+                "publishedAt" => new \DateTime('NOW'),
+                "id" => 3
+            ],
+            4 => [
+                "title" => "La part de l'autre",
+                "author" => "Éric-Emmanuel Schmitt",
+                "publishedAt" => new \DateTime('NOW'),
+                "id" => 4
+            ],
+            5 => [
+                "title" => "Snowman",
+                "author" => "Jo Nesbo",
+                "publishedAt" => new \DateTime('NOW'),
+                "id" => 5
+            ]
+        ];
+        return $this->render('livres.html.twig', ['livres' => $livres]);
+    }
+
+    /**
+     * @Route("/livre/{id}", name="livre")
+     */
+    public function livre($id)
+    {
+        $livres = [
+            1 => [
+                "title" => "Dune",
+                "author" => "Franck Herbert",
+                "publishedAt" => new \DateTime('NOW'),
+                "id" => 1
+            ],
+            2 => [
+                "title" => "Silo",
+                "author" => "Tery Hayes",
+                "publishedAt" => new \DateTime('NOW'),
+                "id" => 2
+            ],
+            3 => [
+                "title" => "Win",
+                "author" => "Harlan Coben",
+                "publishedAt" => new \DateTime('NOW'),
+                "id" => 3
+            ],
+            4 => [
+                "title" => "La part de l'autre",
+                "author" => "Éric-Emmanuel Schmitt",
+                "publishedAt" => new \DateTime('NOW'),
+                "id" => 4
+            ],
+            5 => [
+                "title" => "Snowman",
+                "author" => "Jo Nesbo",
+                "publishedAt" => new \DateTime('NOW'),
+                "id" => 5
+            ]
+        ];
+        if(!array_key_exists($id, $livres)){
+            throw $this->createNotFoundException("Ce livre n'est pas disponible");
+        }
+        $livre = $livres[$id];
+
+        return $this->render('livre.html.twig', ['livre' => $livre]);
+    }
 
 }
